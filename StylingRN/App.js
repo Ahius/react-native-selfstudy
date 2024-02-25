98891b29dff13304598f396845958f39d831b3ea
Hieu Doan <hieudoan280102@gmail.com>
2024-02-25 22:51:11 +0700

    React Native Tutorial - 20 - StyleSheet API

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -3,16 +3,32 @@ import { View, Text, StyleSheet } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>StyleSheet API</Text>
+      <View style={[styles.box, styles.lightBlueBg]}>
+        <Text>LightBlue box</Text>
+      </View>
+      <View style={[styles.box, styles.lightGreenBg]}>
+        <Text>LightGreen box</Text>
+      </View>
     </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'plum',
-    padding:60
+  container: {
+    flex: 1,
+    backgroundColor: 'plum',
+    padding: 60
   },
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
+  lightBlueBg: {
+    backgroundColor: 'lightblue'
+  },
+  lightGreenBg: {
+    backgroundColor: 'lightgreen'
+  }
 })
